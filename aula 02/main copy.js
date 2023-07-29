94e19ebe0715e8a3af1a9d267696567599f29d97
Agedanna <annabeatryz12345@gmail.com>
2023-07-29 16:54:25 -0300

    ex002

---
+++ aula 02/main copy.js
@@ -0,0 +1,12 @@
+var nome = window.prompt("Qual é seu nome?")
+window.alert("É um grande prazer te conhecer, " + nome)
+
+var n1 = Number.parseFloat(window.prompt("Digite um n1: "))
+var n2 = Number.parseFloat(window.prompt("Digite um n2: ")) /* Number.parseFloat trata o dado, ou seja, uma String agora é um dado do tipo Float, podemos usar tbm só Number*/
+
+var media = n1 + n2
+
+window.alert("A soma dos valores é: " + media)
+
+
+
