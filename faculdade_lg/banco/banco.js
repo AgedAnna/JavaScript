fb74a04fa64e2db8ab8200ea015df78b61272b06
Agedanna <annabeatryz12345@gmail.com>
2023-11-22 14:56:59 -0300

    update banco

--- faculdade_lg/banco/banco.js
+++ faculdade_lg/banco/banco.js
@@ -1,110 +1,128 @@
-class ContaBancaria {
-    constructor(agencia, numero, tipo, saldo) {
-        this.agencia = agencia;
-        this.numero = numero;
-        this.tipo = tipo;
-        this.saldo = saldo;
-        this.transacoes = [];
+document.addEventListener('DOMContentLoaded', function () {
+    // Variáveis globais
+    let chart;
+    let selectedData;
+
+    // Carregamento de Dados CSV
+    const csvFileInput = document.getElementById('csvFileInput');
+    csvFileInput.addEventListener('change', handleFile);
+
+    // Seleção de Dados
+    const dataSeriesSelect = document.getElementById('dataSeries');
+    dataSeriesSelect.addEventListener('change', updateChart);
+
+    // Tipos de Gráficos
+    const chartTypeSelect = document.getElementById('chartType');
+    chartTypeSelect.addEventListener('change', updateChart);
+
+    // Opções de Gráficos
+    const startFromZeroCheckbox = document.getElementById('startFromZero');
+    const smoothCurveCheckbox = document.getElementById('smoothCurve');
+    const stackableCheckbox = document.getElementById('stackable');
+    const fillAreaCheckbox = document.getElementById('fillArea');
+    startFromZeroCheckbox.addEventListener('change', updateChart);
+    smoothCurveCheckbox.addEventListener('change', updateChart);
+    stackableCheckbox.addEventListener('change', updateChart);
+    fillAreaCheckbox.addEventListener('change', updateChart);
+
+    // Visualização de Dados
+    const dataPreviewTable = document.getElementById('dataPreview');
+
+    // Exportação do Gráfico
+    function exportChart() {
+        const url = chart.toBase64Image();
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'chart.png';
+        a.click();
     }
 
-    depositar(valor) {
-        this.saldo += valor;
-        this.transacoes.push(`Depósito de R$${valor}`);
+    // Função para carregar o arquivo CSV
+    function handleFile(event) {
+        const file = event.target.files[0];
+        Papa.parse(file, {
+            complete: function (results) {
+                // Preencher a seleção de dados
+                fillDataSeriesSelect(results.data);
+
+                // Exibir a visualização de dados
+                displayDataPreview(results.data);
+            }
+        });
     }
 
-    sacar(valor) {
-        if (valor <= this.saldo) {
-            this.saldo -= valor;
-            this.transacoes.push(`Saque de R$${valor}`);
-        } else {
-            console.log('Saldo insuficiente.');
+    // Função para preencher a seleção de dados
+    function fillDataSeriesSelect(data) {
+        const headers = data[0];
+        dataSeriesSelect.innerHTML = "";
+        headers.forEach((header, index) => {
+            const option = document.createElement('option');
+            option.value = index;
+            option.text = header;
+            dataSeriesSelect.add(option);
+        });
+    }
+
+    // Função para exibir a visualização de dados
+    function displayDataPreview(data) {
+        dataPreviewTable.innerHTML = "";
+        data.slice(0, 5).forEach((row) => {
+            const tr = document.createElement('tr');
+            row.forEach((cell) => {
+                const td = document.createElement('td');
+                td.textContent = cell;
+                tr.appendChild(td);
+            });
+            dataPreviewTable.appendChild(tr);
+        });
+    }
+
+    // Função para atualizar o gráfico
+    function updateChart() {
+        // Obter dados selecionados
+        const selectedColumn = parseInt(dataSeriesSelect.value);
+        selectedData = Papa.parse(csvFileInput.files[0], { header: true }).data.map(row => row[selectedColumn]);
+
+        // Configurar opções do gráfico
+        const chartOptions = {
+            type: chartTypeSelect.value,
+            data: {
+                labels: selectedData.map((_, index) => index + 1),
+                datasets: [{
+                    label: dataSeriesSelect.options[dataSeriesSelect.selectedIndex].text,
+                    data: selectedData,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)', // Cor de fundo
+                    borderColor: 'rgba(75, 192, 192, 1)', // Cor da borda
+                    borderWidth: 1 // Largura da borda
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: startFromZeroCheckbox.checked // Começar eixo Y do zero
+                    }
+                },
+                elements: {
+                    line: {
+                        tension: smoothCurveCheckbox.checked ? 0.4 : 0 // Suavizar curvas
+                    }
+                },
+                plugins: {
+                    filler: {
+                        propagate: fillAreaCheckbox.checked // Preencher áreas sob curvas
+                    },
+                    legend: {
+                        display: false // Ocultar legenda para manter o espaço limpo
+                    }
+                }
+            }
+        };
+
+        // Atualizar ou criar o gráfico
+        if (chart) {
+            chart.destroy();
         }
+        const ctx = document.getElementById('myChart').getContext('2d');
+        chart = new Chart(ctx, chartOptions);
     }
-
-    getHistorico() {
-        return this.transacoes;
-    }
-}
-
-class ContaCorrente extends ContaBancaria {
-    constructor(agencia, numero, saldo, cartaoCredito) {
-        super(agencia, numero, "Conta Corrente", saldo);
-        this.cartaoCredito = cartaoCredito;
-    }
-}
-
-class ContaPoupanca extends ContaBancaria {
-    constructor(agencia, numero, saldo) {
-        super(agencia, numero, "Conta Poupança", saldo);
-    }
-}
-
-class ContaUniversitaria extends ContaBancaria {
-    constructor(agencia, numero, saldo) {
-        super(agencia, numero, "Conta Universitária", saldo);
-    }
-
-    sacar(valor) {
-        if (valor <= 500) {
-            super.sacar(valor);
-        }
-    }
-}
-
-const accounts = [];
-
-document.getElementById("insert-button").addEventListener("click", () => {
-    const accountType = document.getElementById("account-type").value;
-    const agency = document.getElementById("agency").value;
-    const number = document.getElementById("account-number").value;
-    const balance = parseFloat(document.getElementById("balance").value);
-    let account;
-
-    if (accountType === "ContaBancaria") {
-        account = new ContaBancaria(agency, number, accountType, balance);
-    } else if (accountType === "ContaCorrente") {
-        const creditCard = document.getElementById("credit-card").value;
-        account = new ContaCorrente(agency, number, balance, creditCard);
-    } else if (accountType === "ContaPoupanca") {
-        account = new ContaPoupanca(agency, number, balance);
-    } else if (accountType === "ContaUniversitaria") {
-        account = new ContaUniversitaria(agency, number, balance);
-    }
-
-    accounts.push(account);
-    clearFields();
 });
-
-document.getElementById("delete-button").addEventListener("click", () => {
-    const index = document.getElementById("account-list").selectedIndex;
-    if (index !== -1) {
-        accounts.splice(index, 1);
-    }
-    clearFields();
-});
-
-document.getElementById("view-button").addEventListener("click", () => {
-    showAccounts();
-});
-
-function clearFields() {
-    document.getElementById("agency").value = "";
-    document.getElementById("account-number").value = "";
-    document.getElementById("balance").value = "";
-    document.getElementById("credit-card").value = "";
-}
-
-function showAccounts() {
-    const accountList = document.getElementById("account-list");
-    accountList.innerHTML = "";
-    for (const account of accounts) {
-        const listItem = document.createElement("li");
-        listItem.textContent = `Agência: ${account.agencia}, Número: ${account.numero}, Tipo: ${account.tipo}, Saldo: R$${account.saldo}`;
-        const historico = account.getHistorico();
-        if (historico.length > 0) {
-            const historicoText = `Histórico: ${historico.join(', ')}`;
-            listItem.textContent += ` - ${historicoText}`;
-        }
-        accountList.appendChild(listItem);
-    }
-}
